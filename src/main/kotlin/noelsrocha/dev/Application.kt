01c6a6c46326a8a82b85a0aeaef629f8dc4ebc72
noel-srocha <noel.srocha93@gmail.com>
2024-06-25 07:55:39 -0300

    feat: adds database configuration

--- src/main/kotlin/noelsrocha/dev/Application.kt
+++ src/main/kotlin/noelsrocha/dev/Application.kt
@@ -14,6 +14,7 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureDependencyInjection()
+    configureDatabases()
     configureRouting()
     configureTaskRouting()
 }
