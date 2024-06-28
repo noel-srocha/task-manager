e9b68feb444909945f9da17ef8b3a9cab3f48d78
noel-srocha <noel.srocha93@gmail.com>
2024-06-28 05:37:12 -0300

    feat: adjusts methods for data processing from db

--- src/main/kotlin/noelsrocha/dev/tasks/repositories/TaskRepositoryImpl.kt
+++ src/main/kotlin/noelsrocha/dev/tasks/repositories/TaskRepositoryImpl.kt
@@ -1,30 +1,39 @@
 package noelsrocha.dev.tasks.repositories
 
+import noelsrocha.dev.tasks.data.daos.TaskDAO
+import noelsrocha.dev.tasks.data.mappers.daoToModel
+import noelsrocha.dev.tasks.data.mappers.suspendTransaction
+import noelsrocha.dev.tasks.data.tables.TaskTable
 import noelsrocha.dev.tasks.models.Priority
 import noelsrocha.dev.tasks.models.Task
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
 
 class TaskRepositoryImpl : TaskRepository {
-    override suspend fun getTasks(): List<Task> = tasks
-
-    override suspend fun getTasksByPriority(priority: Priority): List<Task> = tasks.filter { it.priority == priority }
-
-    override suspend fun getTaskByName(name: String): Task? = tasks.firstOrNull { it.name.equals(name, ignoreCase = true) }
-
-    override suspend fun saveTask(task: Task) {
-        if (getTaskByName(task.name) != null) {
-            throw IllegalStateException("Cannot duplicate task names!")
-        }
-        tasks.add(task)
+    override suspend fun getTasks(): List<Task> = suspendTransaction {
+        TaskDAO.all().map(::daoToModel)
     }
 
-    override suspend fun deleteTask(name: String): Boolean = tasks.removeIf { it.name == name }
+    override suspend fun getTasksByPriority(priority: Priority): List<Task> = suspendTransaction {
+        TaskDAO.find { (TaskTable.priority eq priority.toString()) }.map(::daoToModel)
+    }
 
-    companion object {
-        val tasks = mutableListOf(
-            Task("Task 1", "Description 1", Priority.LOW),
-            Task("Task 2", "Description 2", Priority.MEDIUM),
-            Task("Task 3", "Description 3", Priority.HIGH),
-            Task("Task 4", "Description 4", Priority.VITAL),
-        )
+    override suspend fun getTaskByName(name: String): Task? = suspendTransaction {
+        TaskDAO.find { (TaskTable.name eq name) }.limit(1).map(::daoToModel).firstOrNull()
+    }
+
+    override suspend fun saveTask(task: Task) : Unit = suspendTransaction {
+        TaskDAO.new {
+            name = task.name
+            description = task.description
+            priority = task.priority.toString()
+        }
+    }
+
+    override suspend fun deleteTask(name: String): Boolean = suspendTransaction {
+        val rowsDeleted = TaskTable.deleteWhere {
+            TaskTable.name eq name
+        }
+        rowsDeleted == 1
     }
 }
